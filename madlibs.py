f71dc7bef1a961f3f710019f520bcd1fcfef17c4
Wmugacha <wilfredmugacha@gmail.com>
2025-02-09 12:59:43 +0800

    madlibs game

---
+++ madlibs.py
@@ -0,0 +1,27 @@
+#This is a simple Mad Libs game that prompts the user for a series of inputs and then fills in the blanks of a pre-written story with them.
+
+print("Mad Libs has started!")
+
+name = input("What should i call you: ")
+
+print(f"Hi {name}, Welcome to the game")
+
+adj1 = input("Enter an adjective(description): ")
+
+adj2 = input("Enter an adjective(description): ")
+
+verb1 = input("Enter a verb/action(ending with ing): ")
+
+place1 = input("Enter a place: ")
+
+place2 = input("Enter a place: ")
+
+print(f"I do not {adj1} going to the school canteen")
+
+print(f"I {adj2} going to the Beijing zoo")
+
+print(f"She went to {place1} and {place2} when she was on vacation")
+
+print(f"I enjoy {verb1} quite alot")
+
+print("Thank you for playing :)")
