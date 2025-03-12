b7fc4b1c3e692697c1ad0abcef89769cdad06bfc
Wmugacha <wilfredmugacha@gmail.com>
2025-03-12 14:30:02 +0800

    recursion

--- recursion.py
+++ recursion.py
@@ -6,9 +6,9 @@ greet()
 
 def fibonacci(idx):
     seq = [0,1]
-    for i in range(idx):
+    for i in range(idx - 1): #running the loop idx times will generate an extra number
         seq.append(seq[-1] + seq[-2])
-    return seq[-2]
+    return seq[-1]
 
 def Fibonacci(idx):
     if idx <= 1:
