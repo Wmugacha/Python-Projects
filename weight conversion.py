182beb56fbd6859aa858c61e02de1aa75e790e92
Wmugacha <wilfredmugacha@gmail.com>
2025-03-19 14:37:43 +0800

    weight conversion program

---
+++ weight conversion.py
@@ -0,0 +1,15 @@
+# Weight Conversion Program
+
+weight = float(input("What is you weight?"))
+unit = input("Kilograms or Pounds? (Kg or P): ")
+
+if unit == "Kg":
+    weight = weight * 2.205
+    units = "Kgs"
+    print(f"your weight is {round(weight, 2)} {units}")
+elif unit == "P":
+    weight = weight / 2.205
+    units = "pounds"
+    print(f"your weight is {round(weight, 2)} {units}")
+else:
+    print(f"{unit} is not a valid weight")
